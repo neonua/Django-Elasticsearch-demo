1216753aeeb5829e46d045cffeed3f5839033862
Igor Gorobets <igor.gorobets@mediasapiens.co>
2017-09-12 15:21:44 +0300

    some changes

--- elastic/search.py
+++ elastic/search.py
@@ -1,5 +1,5 @@
 from elasticsearch_dsl.connections import connections
-from elasticsearch_dsl import DocType, Text, Date, Keyword, Search, tokenizer, analyzer
+from elasticsearch_dsl import DocType, Text, Date, Keyword, Search, tokenizer, analyzer, Q
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
 from . import models
@@ -10,13 +10,15 @@ connections.create_connection(hosts=['127.0.0.1:9200'], timeout=20)
 # simple analyzed. We describe the tokenyzers and filters
 # trigram and nGram search are partial word structure
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-tokenizers.html
-my_analyzer = analyzer('my_analyzer', filter=['lowercase', 'standard', 'snowball'],
-                       tokenizer=tokenizer('trigram', 'nGram', min_gram=3, max_gram=3),)
+my_analyzer = analyzer('my_analyzer',
+                       filter=['lowercase', 'standard', 'snowball'],
+                       tokenizer=tokenizer('standard'),
+                       char_filter=["html_strip"])
 
 
 class DocumentIndex(DocType):
     '''
-    DocType model-like wrapper around model Document
+    Mapping DocType model-like wrapper around model Document
     '''
     title = Text(analyzer=my_analyzer)
     description = Text(analyzer=my_analyzer)
@@ -49,6 +51,6 @@ def search(text):
     '''
     https://elasticsearch-dsl.readthedocs.io/en/latest/search_dsl.html#the-search-object
     '''
-    s = Search().query('multi_match', query=text, fields=['title', 'description', 'text'])
+    s = Search().query('multi_match', query=text, fields=['text', 'title^3', 'description^2'])
     response = s.execute()
     return response
